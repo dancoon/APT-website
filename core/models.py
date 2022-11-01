f3159f3dc22ad197990b36caccfaacd433ab321f
dancoon <dancun.ian@students.jkuat.ac.ke>
2022-11-01 19:43:36 +0300

    Commits

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+POSITION_CHOICES = (
+    ('member','member'),
+    ('developrer', 'developer'),
+    ('instructor', 'instructor')
+)
 # Create your models here.
+class Member(models.Model):
+    fname = models.CharField(max_length = 30)
+    lname = models.CharField(max_length = 30)
+    email = models.CharField(max_length = 200, unique = True)
+    bio = models.TextField(blank = True, null = True)
+    position = models.CharField(max_length = 50, choices = POSITION_CHOICES)
+    is_active = models.BooleanField(default = True)
